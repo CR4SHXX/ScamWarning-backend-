fbc83d445e248676dc81b68fc75ccf6b90b63615
CR4SHXX <radouanischool@gmail.com>
2025-11-04 07:42:42 +0100

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ScamWarning.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Navigation property
+        public ICollection<Warning> Warnings { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ScamWarning.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation properties
+        public ICollection<Warning> Warnings { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
